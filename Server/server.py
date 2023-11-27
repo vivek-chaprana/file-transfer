33ced719edc6ce8d444a01866122bb6c1e7b4514
vivek-chaprana <vivek2003ji@gmail.com>
2023-11-27 15:21:06 +0530

    Update

--- Server/server.py
+++ Server/server.py
@@ -1,42 +1,47 @@
 import socket
 
-# Server configuration
-HOST = '0.0.0.0'  # All available interfaces
+HOST = '0.0.0.0'
 PORT = 5555
 
-# Create a socket object
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Bind the socket to the host and port
-server_socket.bind((HOST, PORT))
-
-# Listen for incoming connections
-server_socket.listen(1)
-
-print(f"Server listening on port {PORT}...")
-
-while True:
-    # Accept incoming connection
-    client_socket, addr = server_socket.accept()
-    print(f"Connection from {addr}")
-
-    ## Receive file name from client
+def receive_file(client_socket):
+    # Receive file name from client
     file_name = client_socket.recv(1024).decode().strip()
-    
+
     # Receive file size from client
-    file_size = int(client_socket.recv(1024).decode())
-    
-    # Open the file received from the client in binary mode
+    file_size_data = b''
+    while len(file_size_data) < 8:
+        file_size_data += client_socket.recv(8 - len(file_size_data))
+
+    file_size = int.from_bytes(file_size_data, byteorder='big')
     received_bytes = 0
+
+    # Open file to write
     with open(file_name, 'wb') as file:
         print(f"Receiving {file_name}...")
         while received_bytes < file_size:
-            # Receive file data in chunks
             data = client_socket.recv(1024)
             received_bytes += len(data)
             file.write(data)
             # Calculate and display progress
             progress = (received_bytes / file_size) * 100
             print(f"Progress: {progress:.2f}%\r", end='')
-        print("\nFile received successfully!")
+
+    print("\nFile received successfully!")
+
+def main():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(1)
+
+    print("Server waiting for connection...")
+
+    client_socket, addr = server_socket.accept()
+    print(f"Connection from {addr}")
+
+    receive_file(client_socket)
+
     client_socket.close()
+    server_socket.close()
+
+if __name__ == "__main__":
+    main()
